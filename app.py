d788f5fe28e2047f7bd520d8789058efce25356d
ogataka55 <ogawatakahiro2107@yahoo.co.jp>
2022-02-06 10:17:41 +0900

    fourth commit(index表示まで)

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-import flask as Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+"""
+書くこと
+    /index で GETリクエスト がきたら
+    index.html というテンプレートをレンダリングする
+"""
+
+
+@app.route("/index")
+def index():
+    customers = [["Bob", 15],
+                 ["tom", 57],
+                 ["Ken", 73]]
+
+    return render_template("index.html", customers=customers)
+
 
 if __name__ == '__main__':
     port = 5000
